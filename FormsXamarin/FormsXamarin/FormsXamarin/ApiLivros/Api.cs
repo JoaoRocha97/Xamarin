f3996e15614b89542809af611a84f55cd6da370c
João Rocha <joao_rocha97@outlook.com>
2017-03-16 13:52:37 -0300

    Commit

---
+++ FormsXamarin/FormsXamarin/FormsXamarin/ApiLivros/Api.cs
@@ -0,0 +1,24 @@
+﻿using FormsXamarin.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FormsXamarin.ApiLivros
+{
+    public static class Api
+    {
+        public static async Task<List<Livro>> GetAsync()
+        {
+            using (var client = new HttpClient())
+            {
+                var json = await client.GetStringAsync("http://mvalivros.azurewebsites.net/api/livros");
+                var livros = JsonConvert.DeserializeObject<List<Livro>>(json);
+                return livros;
+            }
+        }
+    }
+}
