917023ab94a2aa4813b33f41290ea883e300c965
João Rocha <joao_rocha97@outlook.com>
2017-03-16 20:03:13 -0300

    Commit

---
+++ FormsXamarin/FormsXamarin/FormsXamarin/ApiLivros/Api.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace FormsXamarin.ApiLivros
+{
+    static class Api
+    {
+        public static async Task<List<Models.Livro>> GetAsync()
+        {
+            using (var client = new HttpClient())
+            {
+                var json = await client.GetStringAsync("http://mvalivros.azurewebsites.net/api/livros");
+                var livros = JsonConvert.DeserializeObject<List<Models.Livro>>(json);
+                return livros;
+            }
+        }
+    }
+}
---
+++ FormsXamarin/FormsXamarin/FormsXamarin/Models/Livro.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FormsXamarin.Models
+{
+    class Livro : ViewModels.BaseViewModel
+    {
+        private int id;
+        private string nome;
+        private Autor autor;
+
+        public int Id
+        {
+            get
+            {
+                return id;
+            }
+
+            set
+            {
+                id = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string Nome
+        {
+            get
+            {
+                return nome;
+            }
+
+            set
+            {
+                nome = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public Autor Autor
+        {
+            get
+            {
+                return autor;
+            }
+
+            set
+            {
+                autor = value;
+                OnPropertyChanged();
+            }
+        }
+    }
+}
